482c8811327908c0b80dccd6feee5a447531f07c
caianesantos <caianesantos.ph@gmail.com>
2025-08-22 15:11:47 -0300

    adicionando método, atualizando coluna conclusao para status

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 # from datetime import date
 from db import db, cursor
 from flask import Flask, jsonify, request
+from status_task import task_list
+
+
+    
+
+#----------------------ROTAS------------------------------------------
 
 app = Flask(__name__)
 
-#consultar (todos)
+
 @app.route('/tarefas', methods=['GET'])
 def consultar_task():
+    """
+    Consulta todas as tarefas existentes
+    """
     cursor.execute("SELECT * FROM tarefas")
     todos = cursor.fetchall() #pega todas as linhas do banco
     return jsonify(todos)    
 
+@app.route('/tarefas/<int:id>', methods=['GET'])
+def consultar_id_taks(id):
+    """
+    Consulta tarefa pelo id
+    """
+    cursor.execute("SELECT * FROM tarefas WHERE id = %s", (id,))
+    tarefa = cursor.fetchone()
+    return jsonify(tarefa)
 
 @app.route('/tarefas', methods=['POST'])
 def adicionar_task():
+    """
+    Adiciona uma nova tarefa
+    """
     tarefas = request.json # Acessa os dados JSON
     sql = "INSERT INTO tarefas (titulo, descricao, conclusao) VALUES (%s,%s,%s)"
     values = (tarefas["titulo"], tarefas["descricao"], False)
